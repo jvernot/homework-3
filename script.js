f4993b5ccfb8242a94cfb7facb54a03634d2ef07
Jason Vernot <jason.vernot@gmail.com>
2020-03-10 00:31:19 -0400

    final version

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
 
-
 //variables containing all of our possible characters
 var lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
 var upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var allNumbers = '0123456789';
 var allSymbols = '!"#$%&()*+,-./:;=>?@[]^_`{|}~';
 
-// length of password -- changed from a string to an integer
-var length = parseInt(prompt("Please choose a length for your password between 8 and 128 characters."));
-// setting conditons so that the password is not less than 8 or longer than 128 characters
-if (length < 8 || length > 128) {
-  alert("Nope! Please pick a length between 8 and 128 characters.")
-  location.reload();
-} else {
-// confirm whether to include lowercase
-var includeLowercase = confirm("Should the password contain lowercase letters?");
-// confirm whether to include uppercase
-var includeUppercase = confirm("Should the password contain uppercase letters?");
-// confirm whther to include numbers
-var includeNumbers = confirm("Should the password contain numbers?");
-// confirm whether to include symbols
-var includeSymbols = confirm("Should the password contain any symbols?");
 
-// creating a variable to hold the characters for our password
-var characters = "";
-}
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+// Add event listener to generate button
+generateBtn.addEventListener("click", generatePassword);
 
 function generatePassword() {
+  // length of password -- changed from a string to an integer
+  var length = parseInt(prompt("Please choose a length for your password between 8 and 128 characters."));
+  // setting conditons so that the password is not less than 8 or longer than 128 characters
+  if (length < 8 || length > 128) {
+    alert("Nope! Please pick a length between 8 and 128 characters.")
+    location.reload();
+  } else {
+  // confirm whether to include lowercase
+  var includeLowercase = confirm("Should the password contain lowercase letters?");
+  // confirm whether to include uppercase
+  var includeUppercase = confirm("Should the password contain uppercase letters?");
+  // confirm whther to include numbers
+  var includeNumbers = confirm("Should the password contain numbers?");
+  // confirm whether to include symbols
+  var includeSymbols = confirm("Should the password contain any symbols?");
+
+  // creating a variable to hold the characters for our password
+  var characters = "";
+  }
   //check to see if our critera are met and incldue them in the characters variable if they are
   if (includeLowercase) {
     characters += lowerCaseLetters;
@@ -41,6 +45,7 @@ function generatePassword() {
     characters += allSymbols
   }
   if (characters === "") {
+    //stop our user and have them start over if they choose no critera for the password
     alert("This wouldn't be a very secure password! Select at least one criteria.")
     location.reload();
   }
@@ -55,24 +60,4 @@ function generatePassword() {
 
 }
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-// Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword);
-
-generatePassword();
-
-
-
-
-
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
 
